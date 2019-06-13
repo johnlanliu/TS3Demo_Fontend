d5b472e4bb2127d2bd2c18650de0bb0551505ce4
Sean  Lam <seanlam@Seans-MacBook-Pro.local>
2019-06-13 13:04:44 -0700

    added order form and product detail form

---
+++ src/components/module/order/AddOrderForm.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "AddOrderForm"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/module/order/ProductDetailForm.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+	name: "ProductDetailForm"
+  }
+</script>
+
+<style scoped>
+
+</style>
