13995dfde5536c0f43b1e3c0222fa0478b56669d
Sean Lam <seanlam@berkeley.edu>
2019-06-18 16:48:52 -0700

    added files review form

---
+++ src/components/module/order/OrderReviewForm.vue
@@ -0,0 +1,124 @@
+<template>
+    <el-dialog
+        :title="'Order Review'"
+        :center="true"
+        top="15vh"
+        :visible.sync="isOpen"
+        append-to-body="append"
+        @close="resetFields"
+        >
+        <div class="form-box">
+            <el-form ref="form":label-position="labelPosition" :model="form" size="mini" style="padding-right: 0">
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="Bill to" style="font-weight: 900"></el-form-item>
+                        <el-form-item label="Company: ">
+                            <p v-model="billing">{{ billing }}</p>
+                        </el-form-item>
+                        <el-form-item label="Address: ">
+                            <p v-model="billingAddress">{{ billingAddress }}</p>
+                        </el-form-item>
+                        <el-form-item label="Phone Number: ">
+                            <p v-model="billingPhone">{{ billingPhone }}</p>
+                        </el-form-item>
+                        <el-form-item label="Email: ">
+                            <p v-model="billingEmail">{{ billingEmail }}</p>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="Ship to" style="font-weight: 900"></el-form-item>
+                        <el-form-item label="Company: ">
+                            <p v-model="companyName">{{ companyName }}</p>
+                        </el-form-item>
+                        <el-form-item label="Address: ">
+                            <p v-model="shippingAddress">{{ shippingAddress }}</p>
+                        </el-form-item>
+                        <el-form-item label="Phone Number: ">
+                            <p v-model="phone">{{ phone }}</p>
+                        </el-form-item>
+                        <el-form-item label="Email: ">
+                            <p v-model="email">{{ email }}</p>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-form-item label="Order Type: " style="font-weight: 900"></el-form-item>
+                <el-col :span="8" :offset="2">
+                    <el-form-item label="Item" style="font-weight: 900"></el-form-item>
+                </el-col>
+                <el-table
+                    ref="orderDetailTable"
+                    :data="tableData"
+                    v-loading="loading"
+                    border
+                    stripe
+                    highlight-current-row
+                    height="200px"
+                    :row-key="row => row.index"
+                    style="width: 170%; margin-left: 50px"
+                >
+                    <el-table-column label="Product" prop="product" width="120"></el-table-column>
+                    <el-table-column label="QTY" prop="quantity" width="100"></el-table-column>
+                    <el-table-column label="Rate" prop="rate" width="100"></el-table-column>
+                    <el-table-column label="Amount" prop="amount" width="100"></el-table-column>
+                    <el-table-column label="Tax" prop="tax" width="50"></el-table-column>
+                    <el-table-column label="Action" width="130">
+                        <template slot-scope="scope">
+                            <el-dropdown size="mini" type="text" @command="handleCommand">
+                  <span class="el-dropdown-link">
+                  View<i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                                <el-dropdown-menu slot="dropdown">
+                                    <el-dropdown-item command="a">Edit</el-dropdown-item>
+                                    <el-dropdown-item command="b">Delete</el-dropdown-item>
+                                </el-dropdown-menu>
+                            </el-dropdown>
+                        </template>
+                    </el-table-column>
+                </el-table>
+                <el-row>
+                    <el-col :span="8" offset="16">
+                        <el-form-item label="Tax: " style="font-weight: 900"> ${{ form.tax }}</el-form-item>
+                        <el-form-item label="Total: " style="font-weight: 900">${{ form.total }}plus shipping fee</el-form-item>
+                        <el-button type="primary">cancel order</el-button>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </div>
+
+    </el-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'OrderReviewForm',
+    components: {
+
+    },
+    data: function() {
+      return {
+        isOpen: false,
+        loading: false,
+        append: true,
+        labelPosition: 'left',
+        form: {
+          billTo: '',
+          shipTo: '',
+          tax: '_______',
+          total: '____________',
+        }
+      };
+    },
+    methods: {
+      showDialog() {
+        this.isOpen = true;
+      },
+      resetFields() {
+        this.$refs.form.resetFields();
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
