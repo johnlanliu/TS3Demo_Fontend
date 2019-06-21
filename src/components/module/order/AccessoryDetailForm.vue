cd5f3f0957454faa7bc0867ae2dcf58bd6d8aa99
Sean Lam <seanlam@berkeley.edu>
2019-06-21 11:09:04 -0700

    fixed collapsible and spacing

--- src/components/module/order/AccessoryDetailForm.vue
+++ src/components/module/order/AccessoryDetailForm.vue
@@ -8,100 +8,96 @@
             @close="resetFields">
         <div class="accessoryCode">
             <el-form ref="form" :model="form4" size="mini" style="text-align: center">
-                <el-collapse v-mode="form4.activeName" accordion>
+                <el-collapse v-model="form4.activeName" accordion>
                     <el-collapse-item name="1">
                         <template slot="title">
                             {{ form4.accessoryName }}
                         </template>
                         <el-row>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17011')">LA17011</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17021')">LA17021</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA16111')">LA16111</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17031')">LA17031</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17041')">LA17041</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17081')">LA17081</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17051')">LA17051</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA16121 Roo')">LA16121 Roo</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA16121 Coo')">LA16121 Coo</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17121 Roo')">LA17121 Roo</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17121 Aoo')">LA17121 Aoo</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 1" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 1"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('LA17121 Coo')">LA17121 Coo</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 2" :span="6">
                                 <el-form-item>
-                                    <el-button v-if="shown === 0 || shown === 2"
+                                    <el-button
                                                type="primary" style="width: 100px" @click="handleNameClick('BP17021')">BP17021</el-button>
                                 </el-form-item>
                             </el-col>
-                            <el-col :span="6">
+                            <el-col v-if="shown === 0 || shown === 2" :span="6">
                                 <el-form-item>
                                     <el-button
-                                            v-if="shown === 0 || shown === 2"
                                             type="primary" style="width: 100px" @click="handleNameClick('BP18021')">BP18021</el-button>
                                 </el-form-item>
                             </el-col>
-                        </el-row>
-                        <el-row>
-                            <el-button type="primary" style="float: right">Add</el-button>
                         </el-row>
                     </el-collapse-item>
                     <el-collapse-item title="Price" v-if="form4.showPrice" name="2">
@@ -140,13 +136,12 @@ export default {
     return {
       isOpen: false,
       append: true,
-      productName: this.product,
+      loading: false,
       form4: {
         accessoryName: 'Type',
-          activeName: '1',
-          QTY: '',
-          namePicked: false,
-          showPrice: false,
+        activeName: '1',
+        QTY: '',
+        showPrice: false,
       },
     };
   },
@@ -173,30 +168,29 @@ export default {
       this.isOpen = true;
     },
     resetFields() {
-        this.form4.activeName = '1';
-        this.form4.accessoryName = 'Type';
-        this.form4.QTY = '';
-        this.form4.namePicked = false;
-        this.form4.showPrice = false;
+      this.form4.activeName = '1';
+      this.form4.accessoryName = 'Type';
+      this.form4.QTY = '';
+      this.form4.showPrice = false;
       this.$refs.form.resetFields();
     },
-      handleNext(number) {
-          let tempNum = Number(this.form4.activeName);
-          let nextNum = tempNum + number;
-          this.form3.activeName = nextNum.toString();
-      },
-      handleNameClick(num) {
-          if (this.form4.accessoryName === 'Type') {
-              this.form4.productName = num;
-              this.form4.showPrice = true;
-              this.handleNext(2);
-          } else {
-              this.resetFields();
-              this.form4.productName = num;
-              this.form4.showPrice = true;
-              this.handleNext(2);
-          }
-      },
+    handleNext(number) {
+      let tempNum = Number(this.form4.activeName);
+      let nextNum = tempNum + number;
+      this.form4.activeName = nextNum.toString();
+    },
+    handleNameClick(num) {
+      if (this.form4.accessoryName === 'Type') {
+        this.form4.accessoryName = num;
+        this.form4.showPrice = true;
+        this.handleNext(1);
+      } else {
+        this.resetFields();
+        this.form4.accessoryName = num;
+        this.form4.showPrice = true;
+        this.handleNext(1);
+      }
+    },
   },
 };
 </script>
