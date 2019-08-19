1c4e13749ac55922ac5c6e3d63145df18559a18c
18815688119@163.com <18815688119@163.com>
2019-08-19 18:17:14 +0800

    fix bug

--- src/config/env.js
+++ src/config/env.js
@@ -11,7 +11,7 @@ let routerMode = 'hash';
 let baseImgPath;
 
 if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://192.168.0.127:82';
+  baseUrl = 'http://localhost:82';
   // baseUrl = 'http://localhost:82';
   // baseUrl = 'http://anytrek.f3322.net:82';
   // baseUrl = 'http://192.168.156.213:82';
